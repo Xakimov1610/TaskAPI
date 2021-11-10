91da1627a1600659bd3f2df4d2055013c4656733
Nosirbek <68727555+Xakimov1610@users.noreply.github.com>
2021-11-10 21:07:35 +0500

    create migrations and update DB

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +27,8 @@ namespace Task
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Data.TaskDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("TaskConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
